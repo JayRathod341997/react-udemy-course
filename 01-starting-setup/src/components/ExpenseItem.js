3536fd8bddbeaca3e98531a1b774d457dc82fb5c
Jay Rathod <jrathod@measureone.com>
2023-01-11 21:54:45 +0530

    Revert "update value"
    This reverts commit 9798ac283b60e670a5f2404906c3cd06835e0f38.

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -6,7 +6,7 @@ function ExpenseItem() {
       <div>March 28th 2021</div>
       <div className="expense-item__description">
         <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.0</div>
+        <div className="expense-item__price">$294.67</div>
       </div>
     </div>
   );
