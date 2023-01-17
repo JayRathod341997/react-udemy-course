a335534383638ff867dc6aafaed614865b62231f
Jay Rathod <jrathod@measureone.com>
2023-01-17 09:18:10 +0530

    remove comments

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -26,26 +26,6 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <Expense expenses={expenses}></Expense>
-      {/* <ExpenseItemProp
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItemProp>
-      <ExpenseItemProp
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItemProp>
-      <ExpenseItemProp
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItemProp>
-      <ExpenseItemProp
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItemProp> */}
     </div>
   );
 }
