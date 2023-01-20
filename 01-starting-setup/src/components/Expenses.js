63655d5eb363b0771cfe20604b0a534998c42c89
Jay Rathod <jrathod@measureone.com>
2023-01-20 10:18:18 +0530

    Fixed with provided code

--- 01-starting-setup/src/components/Expenses.js
+++ 01-starting-setup/src/components/Expenses.js
@@ -1,31 +1,33 @@
-import ExpenseItemProp from "./ExpenseItemProp";
-function Expense(props) {
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+import "./Expenses.css";
+function Expenses(props) {
   let expenses = props.expenses;
 
   return (
-    <div>
-      <ExpenseItemProp
+    <Card className="expenses">
+      <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItemProp>
-      <ExpenseItemProp
+      ></ExpenseItem>
+      <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpenseItemProp>
-      <ExpenseItemProp
+      ></ExpenseItem>
+      <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpenseItemProp>
-      <ExpenseItemProp
+      ></ExpenseItem>
+      <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-      ></ExpenseItemProp>
-    </div>
+      ></ExpenseItem>
+    </Card>
   );
 }
 
-export default Expense;
+export default Expenses;
