a230a67458cec909de1486acf538454435a48d54
Jay Rathod <jrathod@measureone.com>
2023-01-22 08:53:23 +0530

    make btn size according to device

--- goal-adding-project/src/components/UI/Button/Button.js
+++ goal-adding-project/src/components/UI/Button/Button.js
@@ -3,6 +3,7 @@
 import styled from "styled-components";
 
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -20,6 +21,10 @@ const Button = styled.button`
     background: #ac0e77;
     border-color: #ac0e77;
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+
+  @media (min-width: 768px) {
+    width: auto;
   }
 `;
 
