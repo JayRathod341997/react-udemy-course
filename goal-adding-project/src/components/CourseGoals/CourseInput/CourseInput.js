91dc9f8a08e453df3cebff2dce8ebed0a5fea734
Jay Rathod <jrathod@measureone.com>
2023-01-22 07:57:09 +0530

    Added dynamic inline style

--- goal-adding-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ goal-adding-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -25,18 +25,9 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !validInput ? "red" : "black" }}>
-          Course Goal
-        </label>
-        <input
-          type="text"
-          style={{
-            borderColor: !validInput ? "red" : "#ccc",
-            background: !validInput ? "salmon" : "transparent",
-          }}
-          onChange={goalInputChangeHandler}
-        />
+      <div className={`form-control ${!validInput ? "invalid" : ""}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
