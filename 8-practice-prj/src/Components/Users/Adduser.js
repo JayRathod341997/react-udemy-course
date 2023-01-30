8b2092eb8c756e3a39c777e078673c5059202a38
Jay Rathod <jrathod@measureone.com>
2023-01-30 10:22:10 +0530

    basic css added

--- 8-practice-prj/src/Components/Users/Adduser.js
+++ 8-practice-prj/src/Components/Users/Adduser.js
@@ -1,5 +1,7 @@
 import React from "react";
+import Button from "../UI/Button";
 import Card from "../UI/Card";
+import classes from "./Adduser.module.css";
 
 const Adduser = (props) => {
   const addUserHandler = (event) => {
@@ -7,13 +9,13 @@ const Adduser = (props) => {
   };
 
   return (
-    <Card>
+    <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label>Username </label>
         <input type="text" id="username"></input>
         <label>Age (Years) </label>
         <input type="number" id="age"></input>
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
