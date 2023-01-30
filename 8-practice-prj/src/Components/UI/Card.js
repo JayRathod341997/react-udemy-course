ba052044a603ccd402fc9155b36b09b405305335
Jay Rathod <jrathod@measureone.com>
2023-01-30 09:46:07 +0530

    added Card

--- 8-practice-prj/src/Components/UI/Card.js
+++ 8-practice-prj/src/Components/UI/Card.js
@@ -1,7 +1,7 @@
-
+import classes from "./Card.module.css";
 
 const Card = (props) => {
-  return <div>{props.childen}</div>;
+  return <div className={classes.card}>{props.children}</div>;
 };
 
 export default Card;
