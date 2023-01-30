86c86e058f0528802088f9cd9ede57a027b2bbb9
Jay Rathod <jrathod@measureone.com>
2023-01-30 10:26:47 +0530

    added state for username and age

--- 8-practice-prj/src/Components/Users/Adduser.js
+++ 8-practice-prj/src/Components/Users/Adduser.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./Adduser.module.css";
 
 const Adduser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredUsername, enteredAge);
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label>Username </label>
-        <input type="text" id="username"></input>
+        <input
+          type="text"
+          id="username"
+          onChange={usernameChangeHandler}
+        ></input>
         <label>Age (Years) </label>
-        <input type="number" id="age"></input>
+        <input type="number" id="age" onChange={ageChangeHandler}></input>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
