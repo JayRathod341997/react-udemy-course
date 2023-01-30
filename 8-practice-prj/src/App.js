19d9080e34ef732c5d2c0c2a3a0d93363cf16e0a
Jay Rathod <jrathod@measureone.com>
2023-01-30 10:46:15 +0530

    Added userlist component

--- 8-practice-prj/src/App.js
+++ 8-practice-prj/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Adduser from "./Components/Users/Adduser";
+import UserList from "./Components/Users/UsersList";
 function App() {
   return (
     <div>
       <Adduser />
+      <UserList users={[]} />
     </div>
   );
 }
---
+++ 8-practice-prj/src/Components/Users/UsersList.js
@@ -0,0 +1,17 @@
+import classes from "./UserList.module.css";
+import Card from "../UI/Card";
+const UserList = (props) => {
+  return (
+    <Card className={classes.users}>
+      <ul>
+        {props.users.map((user) => (
+          <li>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+
+export default UserList;
