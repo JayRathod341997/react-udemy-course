93b2de079af12ef47dd0e88994dbbdf6e7e0d53c
Jay Rathod <jrathod@measureone.com>
2023-01-31 11:13:20 +0530

    Added button HeaderCartButton

--- 11-Food-order/src/Components/Layout/Header.js
+++ 11-Food-order/src/Components/Layout/Header.js
@@ -1,13 +1,14 @@
 import classes from "./Header.module.css";
 import { Fragment } from "react";
 import mealsImage from "../../assets/meals.jpeg";
+import Button from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>React-Meals</h1>
-        <button>Cart</button>
+        <Button />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="food img" />
