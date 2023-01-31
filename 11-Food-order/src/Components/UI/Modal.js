2077ab8629893c77e1d28701f06467752b09f007
Jay Rathod <jrathod@measureone.com>
2023-01-31 19:53:47 +0530

    Added overlay for the Cart

---
+++ 11-Food-order/src/Components/UI/Modal.js
@@ -0,0 +1,31 @@
+import classes from "./Modal.module.css";
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModelOverlays = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModelOverlays>{props.children}</ModelOverlays>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
