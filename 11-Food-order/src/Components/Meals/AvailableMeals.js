8807132d81d8e29bedde45b77b3028fe3a65bb4c
Jay Rathod <jrathod@measureone.com>
2023-01-31 13:03:01 +0530

    Added MealItem

--- 11-Food-order/src/Components/Meals/AvailableMeals.js
+++ 11-Food-order/src/Components/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
 import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "../MealItem/MealItem";
 
 const DUMMY_MEALS = [
   {
@@ -28,10 +30,19 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      title={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
