b72f6b5781b55420c4560d97b12a24faa9643a51
Jay Rathod <jrathod@measureone.com>
2023-02-05 19:01:54 +0530

    Added animation to cart

--- 11-Food-order/src/Components/Layout/HeaderCartButton.js
+++ 11-Food-order/src/Components/Layout/HeaderCartButton.js
@@ -1,17 +1,37 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const { items } = cartCtx;
 
   const numberOfCartItmes = cartCtx.items.reduce((currNum, item) => {
     return currNum + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button}  ${
+    btnIsHighlighted ? classes.bump : " "
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
