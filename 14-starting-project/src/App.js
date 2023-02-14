447c0098a79315b7d0fd5e9e229b2caa0a8791ae
Jay Rathod <jrathod@measureone.com>
2023-02-15 01:02:25 +0530

    fixed with contect

--- 14-starting-project/src/App.js
+++ 14-starting-project/src/App.js
@@ -34,17 +34,23 @@ function App() {
     setIsLoading(false);
   }
 
+  let content = <p>No movie found</p>;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = <p>{error}</p>;
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && !error && <p> No Movies</p>}
-        {!isLoading && error && <p>{error}</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
