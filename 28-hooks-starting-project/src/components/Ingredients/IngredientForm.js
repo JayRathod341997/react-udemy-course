e4b453148d10b7fd450bfabc7a40cb10266e5233
Jay Rathod <jrathod@measureone.com>
2023-02-18 15:27:56 +0530

    Added ingradient

--- 28-hooks-starting-project/src/components/Ingredients/IngredientForm.js
+++ 28-hooks-starting-project/src/components/Ingredients/IngredientForm.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from "../UI/Card";
+import "./IngredientForm.css";
 
-const IngredientForm = React.memo(props => {
-  const submitHandler = event => {
+const IngredientForm = React.memo((props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+
+  const submitHandler = (event) => {
     event.preventDefault();
     // ...
+
+    console.log(enteredName, enteredAmount);
+    props.onAddIngredient({ title: enteredName, amount: enteredAmount });
+    setEnteredName("");
+    setEnteredAmount("");
   };
 
   return (
@@ -14,18 +22,32 @@ const IngredientForm = React.memo(props => {
       <Card>
         <form onSubmit={submitHandler}>
           <div className="form-control">
-            <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
-          </div>
+            <label htmlFor="title"> Name </label>{" "}
+            <input
+              type="text"
+              id="title"
+              value={enteredName}
+              onChange={(event) => {
+                setEnteredName(event.target.value);
+              }}
+            />{" "}
+          </div>{" "}
           <div className="form-control">
-            <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
-          </div>
+            <label htmlFor="amount"> Amount </label>{" "}
+            <input
+              type="number"
+              id="amount"
+              value={enteredAmount}
+              onChange={(event) => {
+                setEnteredAmount(event.target.value);
+              }}
+            />
+          </div>{" "}
           <div className="ingredient-form__actions">
-            <button type="submit">Add Ingredient</button>
-          </div>
-        </form>
-      </Card>
+            <button type="submit"> Add Ingredient </button>{" "}
+          </div>{" "}
+        </form>{" "}
+      </Card>{" "}
     </section>
   );
 });
--- 28-hooks-starting-project/src/components/Ingredients/Ingredients.js
+++ 28-hooks-starting-project/src/components/Ingredients/Ingredients.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import { useState } from "react";
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
 
 function Ingredients() {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addingIngredientHandler = (ingredient) => {
+    setUserIngredients((prevIngredient) => [
+      ...prevIngredient,
+      { id: Math.random().toString(), ...ingredient },
+    ]);
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addingIngredientHandler} />
 
       <section>
         <Search />
         {/* Need to add list here! */}
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={() => {}}
+        ></IngredientList>
       </section>
     </div>
   );
