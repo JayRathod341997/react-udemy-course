00b59735a408aa9cca7e8b6e91b921d79f40e5ad
Jay Rathod <jrathod@measureone.com>
2023-02-19 16:09:13 +0530

    Added useRef,setTime , clearTimeout

--- 28-hooks-starting-project/src/components/Ingredients/Search.js
+++ 28-hooks-starting-project/src/components/Ingredients/Search.js
@@ -1,38 +1,46 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
 
 const Search = React.memo((props) => {
   const [enteredText, setEnteredText] = useState("");
+  const inputRef = useRef();
   const { onLoadIngredient } = props;
 
   useEffect(() => {
-    const query =
-      enteredText.length === 0
-        ? ""
-        : `?orderBy="title"&equalTo="${enteredText}" `;
+    const timer = setTimeout(() => {
+      if (enteredText !== inputRef.value) {
+        const query =
+          enteredText.length === 0
+            ? ""
+            : `?orderBy="title"&equalTo="${enteredText}" `;
 
-    fetch(
-      "https://react-hooks-update-8e04b-default-rtdb.firebaseio.com/ingredients.json" +
-        query
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((responseData) => {
-        const loadedIngredientData = [];
-        for (const key in responseData) {
-          loadedIngredientData.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
+        fetch(
+          "https://react-hooks-update-8e04b-default-rtdb.firebaseio.com/ingredients.json" +
+            query
+        )
+          .then((response) => {
+            return response.json();
+          })
+          .then((responseData) => {
+            const loadedIngredientData = [];
+            for (const key in responseData) {
+              loadedIngredientData.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              });
+            }
+            onLoadIngredient(loadedIngredientData);
+            // console.log(loadedIngredientData);
           });
-        }
-        onLoadIngredient(loadedIngredientData);
-        console.log(loadedIngredientData);
-      });
-  }, [enteredText, onLoadIngredient]);
+      }
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [enteredText, onLoadIngredient, inputRef]);
 
   return (
     <section className="search">
@@ -41,6 +49,7 @@ const Search = React.memo((props) => {
           <label>Filter by Title</label>
           <input
             type="text"
+            ref={inputRef}
             value={enteredText}
             onChange={(event) => setEnteredText(event.target.value)}
           />
