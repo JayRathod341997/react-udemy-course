b26f56dbe39ab1128ecc0b18e2446e29a3028e27
Jay Rathod <jrathod@measureone.com>
2023-02-28 09:42:29 +0530

    fix bug

--- 16-custom-forms/src/components/SimpleInput.js
+++ 16-custom-forms/src/components/SimpleInput.js
@@ -4,17 +4,30 @@ const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameIsTouch, setEnteredNameIsTouch] = useState(false);
+
   const nameInputHandler = (event) => {
     setEnteredName(event.target.value);
+    if (event.target.value.trim() !== "") {
+      setEnteredNameIsValid(true);
+    }
+  };
+
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameIsTouch(true);
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+    }
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    setEnteredNameIsTouch(true);
+
     if (enteredName.trim() === "") {
       setEnteredNameIsValid(false);
       return;
     }
-    setEnteredNameIsTouch(true);
+
     console.log(enteredName);
     setEnteredNameIsValid(true);
     setEnteredName("");
@@ -32,6 +45,7 @@ const SimpleInput = (props) => {
           type="text"
           id="name"
           onChange={nameInputHandler}
+          onBlur={nameInputBlurHandler}
           value={enteredName}
         />
         {nameInputIsInvalid && (
