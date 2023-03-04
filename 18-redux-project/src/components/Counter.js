8078f09076d9af008aaf66e1f74bb1dc30d57413
Jay Rathod <jrathod@measureone.com>
2023-03-04 22:16:52 +0530

    replaced with createSlice

--- 18-redux-project/src/components/Counter.js
+++ 18-redux-project/src/components/Counter.js
@@ -1,6 +1,7 @@
 import classes from "./Counter.module.css";
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store/index";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
@@ -8,17 +9,17 @@ const Counter = () => {
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(100));
   };
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounters());
   };
 
   return (
--- 18-redux-project/src/store/index.js
+++ 18-redux-project/src/store/index.js
@@ -1,6 +1,34 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
 const initialState = { counter: 0, showCounter: true };
-const counterReducer = (state = initialState, action) => {
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounters(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
+
+export const counterActions = counterSlice.actions;
+
+export default store;
+/*const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
       counter: state.counter + 1,
@@ -31,5 +59,4 @@ const counterReducer = (state = initialState, action) => {
 };
 
 const store = createStore(counterReducer);
-
-export default store;
+*/
