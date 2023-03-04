fa11426c395907d9cc19cba686816df4021c6a07
Jay Rathod <jrathod@measureone.com>
2023-03-04 12:24:12 +0530

    counter completed

--- 18-redux-project/src/components/Counter.js
+++ 18-redux-project/src/components/Counter.js
@@ -1,15 +1,28 @@
 import classes from "./Counter.module.css";
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
-  
   const toggleCounterHandler = () => {};
   const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
+
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- 18-redux-project/src/store/index.js
+++ 18-redux-project/src/store/index.js
@@ -3,11 +3,11 @@ import { createStore } from "redux";
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "increment") {
     return {
-      counter: action.counter + 1,
+      counter: state.counter + 1,
     };
   } else if (action.type === "decrement") {
     return {
-      counter: action.counter - 1,
+      counter: state.counter - 1,
     };
   } else return state;
 };
