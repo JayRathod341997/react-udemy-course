7a1cd8e2efb00f6b2968a48803c7de718694e318
Jay Rathod <jrathod@measureone.com>
2023-03-04 12:10:51 +0530

    fixed with package.json

--- 18-redux-project/src/components/Counter.js
+++ 18-redux-project/src/components/Counter.js
@@ -1,12 +1,15 @@
 import classes from "./Counter.module.css";
 import React from "react";
-const Counter = () => {
-  const toggleCounterHandler = () => {};
+import { useSelector } from "react-redux";
 
+const Counter = () => {
+  
+  const toggleCounterHandler = () => {};
+  const counter = useSelector((state) => state.counter);
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
