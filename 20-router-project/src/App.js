8dbe5e9536899df83fd323be0ec51a1f51f80416
Jay Rathod <jrathod@measureone.com>
2023-03-08 21:38:36 +0530

    Added Header

--- 20-router-project/src/App.js
+++ 20-router-project/src/App.js
@@ -1,9 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
+import RootLayout from "./pages/Root";
+
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/product", element: <ProductPage /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/product", element: <ProductPage /> },
+    ],
+  },
 ]);
 
 function App() {
