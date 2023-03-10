6f28a2fbb11f8f43e57409c49273d353a8e1280c
Jay Rathod <jrathod@measureone.com>
2023-03-10 23:03:00 +0530

    useNavigate added

--- 20-router-project/src/pages/HomePage.js
+++ 20-router-project/src/pages/HomePage.js
@@ -1,9 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const HomePage = () => {
+  const navigate = useNavigate();
+  const navigateHandler = () => {
+    navigate("/product");
+  };
+
   return (
     <>
       <h1>HomePage</h1>
       <Link to="/product">Go to Product</Link>
+      <div>
+        <button onClick={navigateHandler}> Product</button>
+      </div>
     </>
   );
 };
