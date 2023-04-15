1196582c01032fa092e46b362654e382c4b93465
Jay Rathod <jrathod@measureone.com>
2023-04-15 22:24:25 +0530

    added query.id for dynamic route

--- 23-nextjs-first-app/nextjs-app/pages/news/[newsId].js
+++ 23-nextjs-first-app/nextjs-app/pages/news/[newsId].js
@@ -1,6 +1,10 @@
-//  /root/news/detail
-
+//  /root/news/{detail}
+import { useRouter } from "next/router";
 function DetailPage() {
+
+  const route = useRouter();
+  console.log(route.query.newsId);  // print id of the url = queryparams
+
   return <h1> Detail Page</h1>;
 }
 
