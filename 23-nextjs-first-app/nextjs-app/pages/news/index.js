8099f45925ef3329e8d190b64309198fdde894ff
Jay Rathod <jrathod@measureone.com>
2023-04-15 22:38:09 +0530

    Added link to show one page application

--- 23-nextjs-first-app/nextjs-app/pages/news/index.js
+++ 23-nextjs-first-app/nextjs-app/pages/news/index.js
@@ -1,6 +1,18 @@
+import { Fragment } from "react";
+import Link from "next/link";
 //  /root/news
 function NewsPage() {
-  return <h1> News Page</h1>;
+  return (
+    <Fragment>
+      <h1> News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-the-best">NextJs is best</Link>
+        </li>
+        <li>Something is best</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
