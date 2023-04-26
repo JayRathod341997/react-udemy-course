b90f6ace6596073cff67d4198c67795c95f71736
Jay Rathod <jrathod@measureone.com>
2023-04-26 23:37:47 +0530

    added new TodoItem components

--- 27-react-app-typescript/src/Components/Todos.tsx
+++ 27-react-app-typescript/src/Components/Todos.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Todo from "../models/Todo";
+import TodoItem from "./TodoItem";
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
