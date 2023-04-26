698bd6453afd9f8f6b94d17d8673fff99b1965a6
Jay Rathod <jrathod@measureone.com>
2023-04-26 19:47:33 +0530

    First Todo list project added

---
+++ 27-react-app-typescript/src/App.tsx
@@ -0,0 +1,11 @@
+import Todo from "./Components/Todo";
+
+function App() {
+  return (
+    <div >
+     <Todo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 27-react-app-typescript/src/Components/Todo.tsx
@@ -0,0 +1,8 @@
+const Todo =()=>{
+   return <ul>
+    <li>Frist tast</li>
+    <li>second tast</li>
+   </ul>
+}
+
+export default Todo;
