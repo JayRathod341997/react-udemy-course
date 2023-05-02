1c57ae55b0dda6fe324832debfa700c0a51acfa9
Jay Rathod <jrathod@measureone.com>
2023-05-02 09:22:11 +0530

    lasy loading added

--- 22-starting-project/src/App.js
+++ 22-starting-project/src/App.js
@@ -1,13 +1,16 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import BlogPage, { loader as postsLoader } from './pages/Blog';
-import HomePage from './pages/Home';
-import PostPage, { loader as postLoader } from './pages/Post';
-import RootLayout from './pages/Root';
+// import BlogPage, { loader as postsLoader } from './pages/Blog';
+import HomePage from "./pages/Home";
+// import PostPage, { loader as postLoader } from "./pages/Post";
+import RootLayout from "./pages/Root";
+import { lazy, Suspense } from "react";
 
+const BlogPage = lazy(() => import("./pages/Blog"));
+const PostPage = lazy(() => import("./pages/Post"));
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     children: [
       {
@@ -15,10 +18,28 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: 'posts',
+        path: "posts",
         children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
-          { path: ':id', element: <PostPage />, loader: postLoader },
+          {
+            index: true,
+            element: (
+              <Suspense fallback={<p>Loading .... </p>}>
+                <BlogPage />
+              </Suspense>
+            ),
+            loader: () =>
+              import("./pages/Blog").then((module) => module.loader()),
+          },
+          {
+            path: ":id",
+            element: (
+              <Suspense fallback={<p>Loading .... </p>}>
+                <PostPage />
+              </Suspense>
+            ),
+            loader: (meta) =>
+              import("./pages/Post").then((module) => module.loader(meta)),
+          },
         ],
       },
     ],
