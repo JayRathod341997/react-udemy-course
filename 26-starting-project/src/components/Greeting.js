df007bf990c91b7858160fc568e24003058823ff
Jay Rathod <jrathod@measureone.com>
2023-05-06 20:59:59 +0530

    handled with useState

--- 26-starting-project/src/components/Greeting.js
+++ 26-starting-project/src/components/Greeting.js
@@ -1,9 +1,20 @@
-const Greeting = () => {
-    return <div>
-        <h1>Hello World</h1>
-        <h1>Good to see you.</h1>
-    </div>
-};
+import { useState } from "react";
 
+const Greeting = () => {
+  const [changedText, setChangeText] = useState(false);
+
+  const changedTextHandler = () => {
+    setChangeText(true);
+  };
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h1>Nice , good to see you again!</h1>
+      {!changedText && <p>Paragram tag</p>}
+      {changedText && <p>Changed</p>}
+      <button onClick={changedTextHandler}>Change</button>
+    </div>
+  );
+};
 
 export default Greeting;
--- 26-starting-project/src/components/Greeting.test.js
+++ 26-starting-project/src/components/Greeting.test.js
@@ -1,19 +1,50 @@
-import { render,screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
+import userEvent from "@testing-library/user-event";
 
-describe("Greeting test component", ()=>{
-    test('render hello world',()=>{
-        render(<Greeting/>);
-    
-        const helloWorldElemet = screen.getByText('Hello World');
-        expect(helloWorldElemet).toBeInTheDocument();
+describe("Greeting test component", () => {
+  test("render hello world", () => {
+    render(<Greeting />);
+
+    const helloWorldElemet = screen.getByText("Hello World");
+    expect(helloWorldElemet).toBeInTheDocument();
+  });
+
+  test("render good to see you", () => {
+    render(<Greeting />);
+
+    const helloWorldElemet = screen.getByText("good to see you", {
+      exact: false,
     });
+    expect(helloWorldElemet).toBeInTheDocument();
+  });
 
-    test('render good',()=>{
-        render(<Greeting/>);
-    
-        const helloWorldElemet = screen.getByText('Good',{exact:false});
-        expect(helloWorldElemet).toBeInTheDocument();
+  test("Paragram without button click", () => {
+    render(<Greeting />);
+
+    const helloWorldElemet = screen.getByText("Paragram", {
+      exact: false,
     });
-})
+    expect(helloWorldElemet).toBeInTheDocument();
+  });
 
+  test("Changed with button click", () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    const helloWorldElemet = screen.getByText("Changed");
+    expect(helloWorldElemet).toBeInTheDocument();
+  });
+
+  test("'Paragram' should not be there when button was click", () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    const outputElement = screen.queryByText("Paragram", { exact: false });
+    expect(outputElement).toBeNull();
+  });
+});
