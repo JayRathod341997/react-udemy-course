bf5ad68306f8da170c33f31ef22cc82aca220b9c
Jay Rathod <jrathod@measureone.com>
2023-05-20 14:33:20 +0530

    Added event listener

--- 29-starting-project/src/Components/NewPost.jsx
+++ 29-starting-project/src/Components/NewPost.jsx
@@ -1,12 +1,18 @@
 import classes from "./NewPost.module.css";
-
+import { useState } from "react";
 function NewPost() {
+  const [enteredText, setEnteredText] = useState("");
+
+  const changedBodyhandler = (event) => {
+    setEnteredText(event.target.value);
+  };
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={changedBodyhandler} />
       </p>
+      <p>{enteredText}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
