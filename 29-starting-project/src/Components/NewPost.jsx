f6e135282f55b5f2d7ad8c0f6ca2843c723ab32f
Jay Rathod <jrathod@measureone.com>
2023-05-20 14:45:14 +0530

    lifting state up fixed

--- 29-starting-project/src/Components/NewPost.jsx
+++ 29-starting-project/src/Components/NewPost.jsx
@@ -1,18 +1,12 @@
 import classes from "./NewPost.module.css";
 import { useState } from "react";
-function NewPost() {
-  const [enteredText, setEnteredText] = useState("");
-
-  const changedBodyhandler = (event) => {
-    setEnteredText(event.target.value);
-  };
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changedBodyhandler} />
+        <textarea id="body" required rows={3} onChange={props.onAddPost} />
       </p>
-      <p>{enteredText}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
--- 29-starting-project/src/Components/PostList.jsx
+++ 29-starting-project/src/Components/PostList.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Posts";
 import classes from "./PostList.module.css";
 import NewPost from "./NewPost";
 export default function PostList() {
+  const [enteredText, setEnteredText] = useState("");
+  const bodyChangeHandler = (event) => {
+    setEnteredText(event.target.value);
+  };
+
   return (
     <>
-      <NewPost />
+      <NewPost onAddPost={bodyChangeHandler} />
       <ul className={classes.posts}>
-        <Post author="a1" body="content1" />
+        <Post author="a1" body={enteredText} />
         <Post author="a2" body="content2" />
         <Post author="a3" body="content3" />
       </ul>
