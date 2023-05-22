f5e0b1179aab1110381c160c2b42fb20c015530a
Jay Rathod <jrathod@measureone.com>
2023-05-23 00:18:58 +0530

    Show and hidemodel

--- 29-starting-project/src/Components/Modal.jsx
+++ 29-starting-project/src/Components/Modal.jsx
@@ -3,7 +3,7 @@ import classes from "./Modal.module.css";
 export default function Modal(props) {
   return (
     <>
-      <div className={classes.backdrop}> </div>
+      <div className={classes.backdrop} onClick={props.onClose}></div>
       <dialog open className={classes.modal}>
         {props.children}
       </dialog>
--- 29-starting-project/src/Components/PostList.jsx
+++ 29-starting-project/src/Components/PostList.jsx
@@ -4,16 +4,24 @@ import classes from "./PostList.module.css";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 export default function PostList() {
+  const [visibleModal, setVisibleModal] = useState(true);
   const [enteredText, setEnteredText] = useState("");
   const bodyChangeHandler = (event) => {
     setEnteredText(event.target.value);
   };
 
+  const hideModalHandler = () => {
+    setVisibleModal(false);
+  };
+
   return (
     <>
-      <Modal>
-        <NewPost onAddPost={bodyChangeHandler} />
-      </Modal>
+      {visibleModal && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost onAddPost={bodyChangeHandler} />
+        </Modal>
+      )}
+
       <ul className={classes.posts}>
         <Post author="a1" body={enteredText} />
         <Post author="a2" body="content2" />
