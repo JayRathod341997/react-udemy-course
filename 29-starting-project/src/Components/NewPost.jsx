5b41237a430fd49210424c4b092c2b7266bdedb0
Jay Rathod <jrathod@measureone.com>
2023-05-26 00:21:02 +0530

    refactored NewPost

--- 29-starting-project/src/Components/NewPost.jsx
+++ 29-starting-project/src/Components/NewPost.jsx
@@ -1,15 +1,36 @@
 import classes from "./NewPost.module.css";
 import { useState } from "react";
 function NewPost(props) {
+
+  const [enteredBody,setEnteredBody] = useState();
+  const [enteredAuthor,setEnteredAuthor] = useState();
+  const onBodychangeHandler=(event)=>{
+    setEnteredBody(event.target.value);
+  }
+
+  const onNameChangeHandler=(event)=>{
+    setEnteredAuthor(event.target.value);
+  }
+
+  const submitHandler = (event)=>{
+    event.preventDefault();
+    const personData = {
+      body:enteredBody,
+      author:enteredAuthor
+    }
+    console.log(personData);
+    props.onClose();
+
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={props.onAddPost} />
+        <textarea id="body" required rows={3} onChange={onBodychangeHandler} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={onNameChangeHandler}/>
       </p>
       <p className={classes.actions}>
         <button type="button" onClick={props.onClose}>Cancle</button>
--- 29-starting-project/src/Components/PostList.jsx
+++ 29-starting-project/src/Components/PostList.jsx
@@ -4,21 +4,16 @@ import classes from "./PostList.module.css";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 export default function PostList({ visibleModal, hideModalHandler }) {
-  const [enteredText, setEnteredText] = useState("");
-  const bodyChangeHandler = (event) => {
-    setEnteredText(event.target.value);
-  };
-
+  
   return (
     <>
       {visibleModal && (
         <Modal onClose={hideModalHandler}>
-          <NewPost onAddPost={bodyChangeHandler} onClose={hideModalHandler}/>
+          <NewPost onClose={hideModalHandler}/>
         </Modal>
       )}
 
       <ul className={classes.posts}>
-        <Post author="a1" body={enteredText} />
         <Post author="a2" body="content2" />
         <Post author="a3" body="content3" />
       </ul>
