574b6bf3afdd9500b63510f26d2cef384c4ba8b9
Jay Rathod <jrathod@measureone.com>
2023-05-30 15:00:13 +0530

    added loading fun

--- 29-starting-project/dummy-backend/app.js
+++ 29-starting-project/dummy-backend/app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const { getStoredPosts, storePosts } = require('./data/posts');
+const { getStoredPosts, storePosts } = require("./data/posts");
 
 const app = express();
 
@@ -10,25 +10,25 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   // Attach CORS headers
   // Required when using a detached backend (that runs on a different domain)
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
-app.get('/posts', async (req, res) => {
+app.get("/posts", async (req, res) => {
   const storedPosts = await getStoredPosts();
-  // await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500));
+  await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500));
   res.json({ posts: storedPosts });
 });
 
-app.get('/posts/:id', async (req, res) => {
+app.get("/posts/:id", async (req, res) => {
   const storedPosts = await getStoredPosts();
   const post = storedPosts.find((post) => post.id === req.params.id);
   res.json({ post });
 });
 
-app.post('/posts', async (req, res) => {
+app.post("/posts", async (req, res) => {
   const existingPosts = await getStoredPosts();
   const postData = req.body;
   const newPost = {
@@ -37,7 +37,7 @@ app.post('/posts', async (req, res) => {
   };
   const updatedPosts = [newPost, ...existingPosts];
   await storePosts(updatedPosts);
-  res.status(201).json({ message: 'Stored new post.', post: newPost });
+  res.status(201).json({ message: "Stored new post.", post: newPost });
 });
 
 app.listen(8080);
--- 29-starting-project/src/Components/PostList.jsx
+++ 29-starting-project/src/Components/PostList.jsx
@@ -5,15 +5,20 @@ import NewPost from "./NewPost";
 import Modal from "./Modal";
 export default function PostList({ visibleModal, hideModalHandler }) {
   const [postData, setPostData] = useState([]);
+
+  const [isFetching, setIsFetching] = useState(false);
+
   // fetch("http://localhost:8080/posts")
   //   .then((response) => response.json())
   //   .then((data) => setPostData(data.posts));
   useEffect(() => {
     async function fetchPost() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:8080/posts");
       const resData = await response.json();
       // console.log(resData);
       setPostData(resData.posts);
+      setIsFetching(false);
     }
 
     fetchPost();
@@ -37,11 +42,25 @@ export default function PostList({ visibleModal, hideModalHandler }) {
         </Modal>
       )}
 
-      <ul className={classes.posts}>
-        {postData.map((post) => (
-          <Post key={post.body} author={post.author} body={post.body} />
-        ))}
-      </ul>
+      {!isFetching && postData.length > 0 && (
+        <ul className={classes.posts}>
+          {postData.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+
+      {!isFetching && postData.length === 0 && (
+        <div>
+          <h1>No post available</h1>
+        </div>
+      )}
+
+      {isFetching && (
+        <div>
+          <p>Loading..</p>
+        </div>
+      )}
     </>
   );
 }
